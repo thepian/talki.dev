4e19208ac817305e74a7eaf3674d6547f5162550
Henrik Vendelbo <henrik@thepia.com>
2025-07-24 14:38:40 +0200

    Removed Case Studies samples

--- src/content.config.ts
+++ src/content.config.ts
@@ -31,68 +31,6 @@ const caseStudies = defineCollection({
 		})
 })
 
-const caseStudiesDesign = defineCollection({
-	loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: './src/content/caseStudiesDesign' }),
-	schema: () =>
-		z.object({
-			title: z.string(),
-			description: z.string(),
-			cardImage: z.string(),
-			greyImage: z.string(),
-			heroImage: z.string(),
-			roles: z.array(z.string()),
-			introduction: z.string(),
-			aboutProject: z.object({
-				leftCol: z.string(),
-				rightCol: z.string()
-			}),
-			whatWeDid: z.object({
-				leftCol: z.string(),
-				rightCol: z.string()
-			}),
-			images: z.array(z.string()),
-			clientFeedback: z.array(
-				z.object({
-					blockquote: z.string(),
-					figcaption: z.string(),
-					cite: z.string()
-				})
-			)
-		})
-})
-
-const caseStudiesCode = defineCollection({
-	loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: './src/content/caseStudiesCode' }),
-	schema: () =>
-		z.object({
-			title: z.string(),
-			description: z.string(),
-			cardImage: z.string(),
-			greyImage: z.string(),
-			heroImage: z.string(),
-			roles: z.array(z.string()),
-			introduction: z.string(),
-			aboutProject: z.object({
-				leftCol: z.string(),
-				rightCol: z.string()
-			}),
-			whatWeDid: z.object({
-				leftCol: z.string(),
-				rightCol: z.string()
-			}),
-			images: z.array(z.string()),
-			clientFeedback: z.array(
-				z.object({
-					blockquote: z.string(),
-					figcaption: z.string(),
-					cite: z.string()
-				})
-			)
-		})
-})
-
 export const collections = {
 	caseStudies,
-	caseStudiesDesign,
-	caseStudiesCode
 }
